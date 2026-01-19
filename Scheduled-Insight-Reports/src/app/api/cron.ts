ea04c989cfbd7d2cf38675224499fbd357fe5c82
Helen Su <helenkysu@gmail.com>
2026-01-18 21:35:25 -0500

    (chore): update scheduler, add github workflow

--- Scheduled-Insight-Reports/src/app/api/cron.ts
+++ Scheduled-Insight-Reports/src/app/api/cron.ts
@@ -1,7 +1,8 @@
 import { dbClient, saveError } from "../../lib/database";
 import { generateReport } from "../../lib/generateReport";
 
-export async function loader() {
+export async function GET() {
+  console.log("checking for reports to generate")
   const now = new Date().toISOString();
 
   let newReportId: number | null = null;
@@ -10,19 +11,22 @@ export async function loader() {
     "SELECT * FROM reportconfigs WHERE nextRunAt IS NOT NULL AND nextRunAt <= ?",
     [now]
   );
-
+  
+  
   for (const config of rows) {
     const configId = config.id as number;
+    console.log("generating new report fpr report with config id " + configId )
     try{
         const res = await dbClient.execute(
             "INSERT INTO reports (reportconfigId, status, runStart) VALUES (?, ?, ?)",
             [config.id, 'pending', now]
         );
         newReportId = Number(res.lastInsertRowid);
+        console.log("generating new report with report id " + newReportId )
         await generateReport(newReportId, config, true);
     }
     catch(error){
-        console.error('Error generating report:', error);
+        console.error('Error generating report: ', error);
         await saveError(error, newReportId, configId)
     }
     
